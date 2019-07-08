93b278d5079d09b1a021d56dde0ccdcc1e03fbfc
Oliver <fei.chao.1009@gmail.com>
2019-07-08 18:31:08 +0800

    简略版README.md

---
+++ TryRequests/01.py
@@ -0,0 +1,6 @@
+import requests
+import json
+
+url = 'https://cn-jxnc2-cmcc-v-04.acgvideo.com/upgcxcode/59/45/31124559/31124559-1-15.flv?expires=1562586900&platform=pc&ssig=IuUGU7bNNvVHixppj4LkNQ&oi=1879473336&trid=e84683b5d11c4919a01d75b2f8b05306&nfb=maPYqpoel5MI3qOUX6YpRA==&nfc=1&mid=0'
+
+
